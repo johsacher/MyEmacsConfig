07c59dc330d2921fe5caa746ab5278d821441677
johsacher <johannes.sacher@googlemail.com>
2019-12-03 19:27:18 +0100

    .

--- hlrn_tramp_connect.el
+++ hlrn_tramp_connect.el
@@ -1,39 +1,34 @@
 (defun hlrn-home-tramp ()
-  "Start a terminal-emulator in a new buffer."
   (interactive)
-  (kill-new "TQUuEIBlwLajM2xjj3An")
-  (message "phrase is in clipboard..")
   (find-file "/ssh:blogin:/home/b/beijsach")
 )
 
 (defun hlrn-work1-tramp ()
-  "Start a terminal-emulator in a new buffer."
   (interactive)
-  (kill-new "TQUuEIBlwLajM2xjj3An")
-  (message "phrase is in clipboard..")
   (find-file "/ssh:blogin:/gfs1/work/beijsach")
 )
 		 
 (defun hlrn-work2-tramp ()
-  "Start a terminal-emulator in a new buffer."
   (interactive)
-  (kill-new "TQUuEIBlwLajM2xjj3An")
-  (message "phrase is in clipboard..")
   (find-file "/ssh:blogin:/gfs2/work/beijsach")
 )
 
 (defun hlrn-work1-tramp-scp ()
-  "Start a terminal-emulator in a new buffer."
   (interactive)
-  (kill-new "TQUuEIBlwLajM2xjj3An")
-  (message "phrase is in clipboard..")
   (find-file "/scp:blogin:/gfs1/work/beijsach")
 )
 
 (defun hlrn-work1-tramp-sshx ()
-  "Start a terminal-emulator in a new buffer."
   (interactive)
-  (kill-new "TQUuEIBlwLajM2xjj3An")
-  (message "phrase is in clipboard..")
   (find-file "/sshx:blogin:/gfs1/work/beijsach")
+)
+
+(defun mathecluster-home-tramp ()
+  (interactive)
+  (find-file "/ssh:mathe:/homes2/ifvt/sacher")
+)
+
+(defun mathecluster-work-tramp ()
+  (interactive)
+  (find-file "/ssh:mathe:/net/work/sacher")
 )
