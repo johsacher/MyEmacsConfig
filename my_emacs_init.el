98cbf8e97fb7394a3b721ce13e8771249b8f7c1a
johsacher <johannes.sacher@googlemail.com>
2020-03-21 19:58:29 +0100

    .

--- my_emacs_init.el
+++ my_emacs_init.el
@@ -352,6 +352,56 @@
              ))
 (require 'org-install)
 
+;; show distribution of clocked time per tag
+(require 'org-table)
+(require 'org-clock)
+
+(defun clocktable-by-tag/shift-cell (n)
+  (let ((str ""))
+    (dotimes (i n)
+      (setq str (concat str "| ")))
+    str))
+
+(defun clocktable-by-tag/insert-tag (params)
+  (let ((tag (plist-get params :tags)))
+    (insert "|--\n")
+    (insert (format "| %s | *Tag time* |\n" tag))
+    (let ((total 0))
+  (mapcar
+       (lambda (file)
+     (let ((clock-data (with-current-buffer (find-file-noselect file)
+                 (org-clock-get-table-data (buffer-name) params))))
+       (when (> (nth 1 clock-data) 0)
+         (setq total (+ total (nth 1 clock-data)))
+         (insert (format "| | File *%s* | %.2f |\n"
+                 (file-name-nondirectory file)
+                 (/ (nth 1 clock-data) 60.0)))
+         (dolist (entry (nth 2 clock-data))
+           (insert (format "| | . %s%s | %s %.2f |\n"
+                   (org-clocktable-indent-string (nth 0 entry))
+                   (nth 1 entry)
+                   (clocktable-by-tag/shift-cell (nth 0 entry))
+                   (/ (nth 3 entry) 60.0)))))))
+       (org-agenda-files))
+      (save-excursion
+    (re-search-backward "*Tag time*")
+    (org-table-next-field)
+    (org-table-blank-field)
+    (insert (format "*%.2f*" (/ total 60.0)))))
+    (org-table-align)))
+
+(defun org-dblock-write:clocktable-by-tag (params)
+  (insert "| Tag | Headline | Time (h) |\n")
+  (insert "|     |          | <r>  |\n")
+  (let ((tags (plist-get params :tags)))
+    (mapcar (lambda (tag)
+          (setq params (plist-put params :tags tag))
+          (clocktable-by-tag/insert-tag params))
+        tags)))
+
+(provide 'clocktable-by-tag)
+
+
 ;;** evaluate code snippets in org
 ;;*** don t confirm every time
 (setq org-confirm-babel-evaluate nil)
@@ -407,12 +457,18 @@
   (my-toggle-marker-around-region "*" "\*"  "*" "\*")
   )
 
+(defun org-toggle-red-region ()
+  (interactive)
+  (my-toggle-marker-around-region "=" "="  "=" "=")
+  )
+
 (defun org-toggle-italic-region ()
   (interactive)
   (my-toggle-marker-around-region "/" "\/" "/" "\/")
   )
 (evil-leader/set-key "ob" 'org-toggle-bold-region)
 (evil-leader/set-key "oi" 'org-toggle-italic-region)
+(evil-leader/set-key "or" 'org-toggle-red-region)
 
 (defun my-toggle-marker-around-region (marker-begin marker-begin-regex marker-end marker-end-regex)
   (setq begin (region-beginning))
@@ -444,6 +500,10 @@
         )
       )
   )
+
+;;** enable enumerations with a./b./c.
+(setq org-list-allow-alphabetical t)
+
 
 ;;** org-ref (--> citation management & pdflatex export)
 (require 'org-ref)
