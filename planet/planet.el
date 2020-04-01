b6e5fe98c65db9e62d750b6f563158da68f3878f
johsacher <johannes.sacher@googlemail.com>
2020-04-01 23:04:10 +0200

    .

--- planet/planet.el
+++ planet/planet.el
@@ -1103,12 +1103,12 @@ date)
   (setq planet-git-save-switch nil)
   (message "planet-git-save turned off.")
   (if (not planet-git-save-switch)
-    (progn
-      (add-hook 'after-save-hook 'planet-git-sync-file)
-      (setq planet-git-save-switch t)
-      (message "planet-git-save turned on.")
-      )
-  )
+      (progn
+        (add-hook 'after-save-hook 'planet-git-sync-file)
+        (setq planet-git-save-switch t)
+        (message "planet-git-save turned on.")
+        )
+    )
   )
 
 (defun planet-git-sync-up-file ()
