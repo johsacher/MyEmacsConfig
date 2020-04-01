a1bbd6c17edc4cce8ea9b2e0c61fb8f66ca07885
johsacher <johannes.sacher@googlemail.com>
2020-04-01 23:34:18 +0200

    .

--- planet/planet.el
+++ planet/planet.el
@@ -1125,6 +1125,7 @@ date)
 (defun planet-revert-all-planet-buffers ()
   (interactive)
   (if (bound-and-true-p planet-mode)   ;; https://stackoverflow.com/questions/10088168/how-to-check-whether-a-minor-mode-e-g-flymake-mode-is-on
+      (revert-buffer)
 
 
   )
