5de92421aab5aca1cfb93e46a41a79e4926545ee
johsacher <johannes.sacher@googlemail.com>
2020-04-02 02:23:34 +0200

    .

--- planet/planet.el
+++ planet/planet.el
@@ -1118,6 +1118,7 @@ date)
 
 (defun planet-git-sync-down-revert-file ()
   (interactive)
+  (message (concat "git-pulled."))
   (shell-command "git pull")
   (message (concat "git-pulled."))
   (revert-buffer)
