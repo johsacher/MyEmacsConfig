034872198db732395ca79b5c1db25111766453b7
johsacher <johannes.sacher@googlemail.com>
2020-04-02 02:27:03 +0200

    .

--- planet/planet.el
+++ planet/planet.el
@@ -1110,7 +1110,7 @@ date)
 
 (defun planet-git-sync-up-file ()
   (interactive)
-  (if planet-detect-if-planet-file
+  (if (planet-detect-if-planet-file)
   (setq command-string (concat "git add " buffer-file-name " && git commit -m '.' && git push"))
   (async-shell-command command-string)
   (message (concat "git-saved file: " buffer-file-name " (git pushed)." ))
