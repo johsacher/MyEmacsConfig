9beccb811331f4d1a367a642a6795ef225a09350
johsacher <johannes.sacher@googlemail.com>
2020-11-25 09:58:02 +0400

    .

--- my_emacs_init.el
+++ my_emacs_init.el
@@ -3408,3 +3408,23 @@ region, clear header."
 
 ;; ** expand-region -> evil-mode shortcut -> visual mode map: "v" -> expand region / instead of exit visual mode
 (define-key evil-visual-state-map (kbd "v") 'er/expand-region)
+
+;; * sudo-edit
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+
+;; * draft-horse-term
+(defun draft-horse-term ()
+  (interactive)
+
+  )
