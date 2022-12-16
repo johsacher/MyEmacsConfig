ece49202f308d2da9f3e524756c4efd97da3b779
Johannes Sacher <johannes.sacher@googlemail.com>
2022-12-16 14:56:00 +0100

    .

--- my_emacs_init_doomtransfer2021.el
+++ my_emacs_init_doomtransfer2021.el
@@ -5018,6 +5018,38 @@ and `C-x' being marked as a `term-escape-char'."
   (insert "\U000003B6"))
 (js/specialchardef "g z" #'js/insert-unicode-zeta)
 
+(defun js/insert-unicode-eta ()
+  ;; η
+  (interactive)
+  (insert "\U000003B7"))
+(js/specialchardef "g h" #'js/insert-unicode-eta)
+
+;; latin equivalents for greek letters (➜ choose my key-combos)
+;; Α 	α 	Alpha 	a
+;; Β 	β 	Beta 	b
+;; Γ 	γ 	Gamma 	g
+;; Δ 	δ 	Delta 	d
+;; Ε 	ε 	Epsilon 	e
+;; Ζ 	ζ 	Zeta 	z
+;; Η 	η 	Eta 	h
+;; Θ 	θ 	Theta 	th
+;; Ι 	ι 	Iota 	i
+;; Κ 	κ 	Kappa 	k
+;; Λ 	λ 	Lambda 	l
+;; Μ 	μ 	Mu 	m
+;; Ν 	ν 	Nu 	n
+;; Ξ 	ξ 	Xi 	x
+;; Ο 	ο 	Omicron 	o
+;; Π 	π 	Pi 	p
+;; Ρ 	ρ 	Rho 	r
+;; Σ 	σ,ς * 	Sigma 	s
+;; Τ 	τ 	Tau 	t
+;; Υ 	υ 	Upsilon 	u
+;; Φ 	φ 	Phi 	ph
+;; Χ 	χ 	Chi 	ch
+;; Ψ 	ψ 	Psi 	ps
+;; Ω 	ω 	Omega 	o
+
 (defun js/insert-unicode-sub-i ()
   ;; ᵢ
   (interactive)
